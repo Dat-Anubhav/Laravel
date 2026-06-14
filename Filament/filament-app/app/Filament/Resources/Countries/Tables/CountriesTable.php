53a9bd928e8f69587abfce3b423535f631468adc
Anubhav Srivastav <anubhavsrivastavbst@gmail.com>
2026-06-14 15:13:36 +0530

    Add dependent selects for country, state, and city

--- Filament/filament-app/app/Filament/Resources/Countries/Tables/CountriesTable.php
+++ Filament/filament-app/app/Filament/Resources/Countries/Tables/CountriesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Countries\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class CountriesTable
@@ -13,7 +14,7 @@ class CountriesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make("name")
             ])
             ->filters([
                 //
--- Filament/filament-app/app/Filament/Resources/States/Tables/StatesTable.php
+++ Filament/filament-app/app/Filament/Resources/States/Tables/StatesTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\States\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class StatesTable
@@ -13,7 +14,8 @@ class StatesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make("name"),
+                TextColumn::make("country.name")
             ])
             ->filters([
                 //
